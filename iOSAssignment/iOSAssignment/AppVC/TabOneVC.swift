f9c931920d8cc08f27c08c536941f3c088173bc9
matrix01 <milon06@gmail.com>
2016-09-04 11:57:36 +0600

    Project initialized

---
+++ iOSAssignment/iOSAssignment/AppVC/TabOneVC.swift
@@ -0,0 +1,35 @@
+//
+//  TabOneVC.swift
+//  iOSAssignment
+//
+//  Created by Milan Mia on 9/4/16.
+//  Copyright © 2016 Milan Mia. All rights reserved.
+//
+
+import UIKit
+
+class TabOneVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
